e107291de3c2795146495a6658f226c402412e8b
vazirabonu [FE16] <102415721+vazirabonu23@users.noreply.github.com>
2022-12-09 15:06:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+
+
+//     function setInfo(){
+//         let student1 = {
+//        name: prompt('Ism kiriting'),
+//        surname : prompt('Familiya kiriting'),
+//        age : prompt('Yosh kiriting'),
+//        object: {student1:""
+//        }}
+//        console.log(student1);
+   
+//        let student2 = {
+//        name: prompt('Ism kiriting'),
+//        surname : prompt('Familiya kiriting'),
+//        age : prompt('Yosh kiriting'),
+//        object: {student1:""
+//        }}
+//        console.log(student2);
+   
+//        let student3 = {
+//        name: prompt('Ism kiriting'),
+//        surname : prompt('Familiya kiriting'),
+//        age : prompt('Yosh kiriting'),
+//        object: {student1:""
+//        }}
+//        console.log(student3);
+   
+//        let student4 = {
+//        name: prompt('Ism kiriting'),
+//        surname : prompt('Familiya kiriting'),
+//        age : prompt('Yosh kiriting'),
+//        object: {student1:""
+//        }}
+//        console.log(student4);
+   
+   
+//        let student5 = {
+//            name: prompt('Ism kiriting'),
+//            surname : prompt('Familiya kiriting'),
+//            age : prompt('Yosh kiriting'),
+//            object: {student1:""
+//                }}
+//                console.log(student5);
+//    }
+//    setInfo()
+
+
+
+
+
+
+
+
+
+
+
+// 1. 
+// let x = 2
+// let n = 3
+// function pow() {
+//     return x ** n
+// }
+// pow()
+// console.log(pow(x, n))
+
+
+
+// 2.
+// let number = 3.45781
+// function floor() {
+//     return ~number * -1 -1
+// }
+// console.log(floor(number))
+
+
+
+// 5.
+// let a = 16
+// let b = 2
+// function sqrt() {
+//     return a ** (1/b);
+// }
+// sqrt()
+// console.log(sqrt(a, b));
+
+// let x = 8
+// let n = 3
+// function sqrt() {
+//     return x ** (1/n);
+// }
+// sqrt()
+// console.log(sqrt(x, n));
+
+
+ 
+
+
+
+
